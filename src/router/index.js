9c7f382f56ca1a6e1551bf9a4309973a89d7c938
Atinder-Pal <atty.pal46@gmail.com>
2021-01-07 14:19:10 -0700

    Remove scaffolded view folder

--- src/router/index.js
+++ src/router/index.js
@@ -1,17 +1,11 @@
 import { createRouter, createWebHistory } from '@ionic/vue-router';
-import { RouteRecordRaw } from 'vue-router';
-import Home from '../views/Home.vue'
+
 
 const routes= [
   {
     path: '/',
     redirect: '/home'
-  },
-  {
-    path: '/home',
-    name: 'Home',
-    component: Home
-  }
+  } 
 ]
 
 const router = createRouter({
