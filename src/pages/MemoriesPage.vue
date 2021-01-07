200ccca5205717f3b355f1e8db149bf8abed8d4d
Atinder-Pal <atty.pal46@gmail.com>
2021-01-07 14:48:12 -0700

    Created MemoriesPage as the main page and Added static list items;

---
+++ src/pages/MemoriesPage.vue
@@ -0,0 +1,34 @@
+<template>
+    <ion-page>
+        <ion-header>
+            <ion-toolbar>
+                <ion-title>
+                    All Memories
+                </ion-title>
+            </ion-toolbar>            
+        </ion-header>
+        <ion-content>
+            <ion-list>
+                <ion-item>Max</ion-item>
+                <ion-item>Manuel</ion-item>
+                <ion-item>Alice</ion-item>
+                <ion-item>Bob</ion-item>
+            </ion-list>
+        </ion-content>
+    </ion-page>
+</template>
+<script>
+import {IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonList, IonItem} from '@ionic/vue';
+
+export default {
+    components: {
+       IonPage,
+       IonHeader,
+       IonToolbar,
+       IonTitle,
+       IonContent,
+       IonList,
+       IonItem
+    }
+}
+</script>
