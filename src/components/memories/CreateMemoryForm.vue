fe632919c1611d1b6b63cd2b7145c5404129a7da
Atinder-Pal <atty.pal46@gmail.com>
2021-01-08 06:51:14 -0700

    Created component CreateMemoryForm to disply form in AddMemoryPage;

---
+++ src/components/memories/CreateMemoryForm.vue
@@ -0,0 +1,44 @@
+<template>
+  <form class="ion-padding">
+    <ion-list>
+      <ion-item>
+        <ion-label position="floating">Title</ion-label>
+        <ion-input type="text" />
+      </ion-item>
+    
+      <ion-item>
+        <ion-label position="floating">Image URL</ion-label>
+        <ion-input type="url" />
+      </ion-item>
+    
+      <ion-item>
+        <ion-label position="floating">Description</ion-label>
+        <ion-textarea type="text" />
+      </ion-item>
+    </ion-list>    
+    <ion-button type="submit" expand="block">Save</ion-button>
+  </form>
+</template>
+
+<script>
+import {
+  IonList,
+  IonItem,
+  IonLabel,
+  IonInput,
+  IonTextarea,
+  IonButton,
+  
+} from "@ionic/vue";
+
+export default {
+    components: {
+    IonList,
+    IonItem,
+    IonLabel,
+    IonInput,
+    IonTextarea,
+    IonButton   
+  } 
+};
+</script>
--- src/pages/AddMemoryPage.vue
+++ src/pages/AddMemoryPage.vue
@@ -1,9 +1,14 @@
 <template>
-    
+    <base-layout pageTitle="Add a Memory" pageDefaultBackLink="/memories">
+        <create-memory-form></create-memory-form>
+    </base-layout>
 </template>
 
 <script>
+import CreateMemoryForm from '../components/memories/CreateMemoryForm.vue';
+
 export default {
+  components: { CreateMemoryForm },
     
 }
 </script>
