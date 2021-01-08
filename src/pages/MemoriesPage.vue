8a6b80b90c29b1548c31901ea00e17f4b08f4128
Atinder-Pal <atty.pal46@gmail.com>
2021-01-07 17:40:10 -0700

    Moved memories array to global store;

--- src/pages/MemoriesPage.vue
+++ src/pages/MemoriesPage.vue
@@ -17,29 +17,12 @@ import {IonList, IonItem, IonImg, IonThumbnail, IonLabel} from '@ionic/vue';
 
 
 export default {
-    data() {
-        return {
-            memories: [
-                {
-                    id:"m1",
-                    image: "https://cdn.pixabay.com/photo/2017/02/01/22/02/mountain-landscape-2031539__340.jpg",
-                    title: "A trip into the mountains",
-                    description: "It was a really nice trip!"
-                },
-                 {
-                    id:"m2",
-                    image: "https://cdn.pixabay.com/photo/2020/12/14/13/51/jasper-national-park-5830929_960_720.jpg",
-                    title: "A trip to Jasper National Park",
-                    description: "It was a quick break from TechCareers!"
-                }, {
-                    id:"m3",
-                    image: "https://cdn.pixabay.com/photo/2017/01/11/11/33/cake-1971552_960_720.jpg",
-                    title: "Celebrated New Year by baking a cake",
-                    description: "It was a nice experience baking again after a long time!"
-                },
-            ]
-        }
-    },
+   computed: {
+       memories() {
+           return this.$store.getters.memories;
+       }
+   },
+    
     components: {      
        IonList,
        IonItem,
--- src/store/index.js
+++ src/store/index.js
@@ -1,3 +1,35 @@
 import { createStore } from 'vuex';
 
-const store= createStore();
+const store= createStore({
+    state(){
+        return {
+            memories: [
+                {
+                    id:"m1",
+                    image: "https://cdn.pixabay.com/photo/2017/02/01/22/02/mountain-landscape-2031539__340.jpg",
+                    title: "A trip into the mountains",
+                    description: "It was a really nice trip!"
+                },
+                 {
+                    id:"m2",
+                    image: "https://cdn.pixabay.com/photo/2020/12/14/13/51/jasper-national-park-5830929_960_720.jpg",
+                    title: "A trip to Jasper National Park",
+                    description: "It was a quick break from TechCareers!"
+                }, {
+                    id:"m3",
+                    image: "https://cdn.pixabay.com/photo/2017/01/11/11/33/cake-1971552_960_720.jpg",
+                    title: "Celebrated New Year by baking a cake",
+                    description: "It was a nice experience baking again after a long time!"
+                },
+            ],
+            
+        };
+    },
+    getters: {
+        memories(state) {
+            return state.memories;
+        }
+    }
+});
+
+export default store;
